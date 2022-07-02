8ead5594055414fe693e6855ae62ada4a24469d7
juni <woajun@naver.com>
2022-07-02 16:29:18 +0900

    KakaoMap setup

--- frontend/src/components/KakaoMap.vue
+++ frontend/src/components/KakaoMap.vue
@@ -1,5 +1,5 @@
-<script lang="ts">
-import { defineComponent, onMounted, ref } from "vue";
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
 
 declare global {
   interface Window {
@@ -14,123 +14,107 @@ interface Kakao {
 interface Map {
   load: (map: any) => void;
 }
-export default defineComponent({
-  name: "KakaoMap",
-  setup(props, context) {
-    const map = ref<Map>();
-    const markers = ref<number[]>();
-    const infowindow = ref();
+const map = ref<Map>();
+const markers = ref<number[]>();
+const infowindow = ref();
 
-    const initMap = function () {
-      const container = document.getElementById("map");
-      const options = {
-        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-        level: 5,
-      };
+//staticData
+const markerPositions1 = [
+  [33.452278, 126.567803],
+  [33.452671, 126.574792],
+  [33.451744, 126.572441],
+];
+const markerPositions2 = [
+  [37.499590490909185, 127.0263723554437],
+  [37.499427948430814, 127.02794423197847],
+  [37.498553760499505, 127.02882598822454],
+  [37.497625593121384, 127.02935713582038],
+  [37.49629291770947, 127.02587362608637],
+  [37.49754540521486, 127.02546694890695],
+  [37.49646391248451, 127.02675574250912],
+];
 
-      //지도 객체를 등록합니다.
-      //지도 객체는 반응형 관리 대상이 아니므로 initMap에서 선언합니다.
-      map.value = new window.kakao.maps.Map(container, options);
+const initMap = function () {
+  const container = document.getElementById("map");
+  const options = {
+    center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+    level: 5,
+  };
+
+  //지도 객체를 등록합니다.
+  //지도 객체는 반응형 관리 대상이 아니므로 initMap에서 선언합니다.
+  map.value = new window.kakao.maps.Map(container, options);
+};
+
+const changeSize = function (size: number) {
+  const container = document.getElementById("map");
+  if (!container) return;
+  container.style.width = `${size}px`;
+  container.style.height = `${size}px`;
+  map.value?.relayout();
+};
+
+const displayMarker = function (markerPositions: number[][]) {
+  if (markers.value && markers.value.length > 0) {
+    markers.value.forEach((marker) => marker.setMap(null));
+  }
+
+  const positions = markerPositions.map(
+    (position) => new window.kakao.maps.LatLng(...position)
+  );
+
+  if (positions.length > 0) {
+    markers.value = positions.map(
+      (position) =>
+        new window.kakao.maps.Marker({
+          map: map.value,
+          position,
+        })
+    );
+
+    const bounds = positions.reduce(
+      (bounds, latlng) => bounds.extend(latlng),
+      new window.kakao.maps.LatLngBounds()
+    );
+
+    map.value?.setBounds(bounds);
+  }
+};
+
+const displayInfoWindow = function () {
+  if (infowindow.value && infowindow.value.getMap()) {
+    //이미 생성한 인포윈도우가 있기 때문에 지도 중심좌표를 인포윈도우 좌표로 이동시킨다.
+    map.value?.setCenter(infowindow.value.getPosition());
+    return;
+  }
+
+  var iwContent = '<div style="padding:5px;">Hello World!</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+    iwPosition = new window.kakao.maps.LatLng(33.450701, 126.570667), //인포윈도우 표시 위치입니다
+    iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+
+  infowindow.value = new window.kakao.maps.InfoWindow({
+    map: map.value, // 인포윈도우가 표시될 지도
+    position: iwPosition,
+    content: iwContent,
+    removable: iwRemoveable,
+  });
+
+  map.value?.setCenter(iwPosition);
+};
+
+onMounted(() => {
+  if (window.kakao && window.kakao.maps) {
+    initMap();
+  } else {
+    const script = document.createElement("script");
+    /* global kakao */
+    script.onload = () => {
+      return window.kakao.maps.load(initMap);
     };
-
-    const changeSize = function (size: number) {
-      const container = document.getElementById("map");
-      if (!container) return;
-      container.style.width = `${size}px`;
-      container.style.height = `${size}px`;
-      map.value?.relayout();
-    };
-
-    const displayMarker = function (markerPositions: number[][]) {
-      if (markers.value && markers.value.length > 0) {
-        markers.value.forEach((marker) => marker.setMap(null));
-      }
-
-      const positions = markerPositions.map(
-        (position) => new window.kakao.maps.LatLng(...position)
-      );
-
-      if (positions.length > 0) {
-        markers.value = positions.map(
-          (position) =>
-            new window.kakao.maps.Marker({
-              map: map.value,
-              position,
-            })
-        );
-
-        const bounds = positions.reduce(
-          (bounds, latlng) => bounds.extend(latlng),
-          new window.kakao.maps.LatLngBounds()
-        );
-
-        map.value?.setBounds(bounds);
-      }
-    };
-
-    const displayInfoWindow = function () {
-      if (infowindow.value && infowindow.value.getMap()) {
-        //이미 생성한 인포윈도우가 있기 때문에 지도 중심좌표를 인포윈도우 좌표로 이동시킨다.
-        map.value?.setCenter(infowindow.value.getPosition());
-        return;
-      }
-
-      var iwContent = '<div style="padding:5px;">Hello World!</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-        iwPosition = new window.kakao.maps.LatLng(33.450701, 126.570667), //인포윈도우 표시 위치입니다
-        iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
-
-      infowindow.value = new window.kakao.maps.InfoWindow({
-        map: map.value, // 인포윈도우가 표시될 지도
-        position: iwPosition,
-        content: iwContent,
-        removable: iwRemoveable,
-      });
-
-      map.value?.setCenter(iwPosition);
-    };
-
-    onMounted(() => {
-      if (window.kakao && window.kakao.maps) {
-        initMap();
-      } else {
-        const script = document.createElement("script");
-        /* global kakao */
-        script.onload = () => {
-          return window.kakao.maps.load(initMap);
-        };
-        script.src =
-          "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=6980627efdafc9b33ee3f2e602c8f9da";
-        document.head.appendChild(script);
-      }
-    });
-    return {
-      //data
-      map,
-      //staticData
-      markerPositions1: [
-        [33.452278, 126.567803],
-        [33.452671, 126.574792],
-        [33.451744, 126.572441],
-      ],
-      markerPositions2: [
-        [37.499590490909185, 127.0263723554437],
-        [37.499427948430814, 127.02794423197847],
-        [37.498553760499505, 127.02882598822454],
-        [37.497625593121384, 127.02935713582038],
-        [37.49629291770947, 127.02587362608637],
-        [37.49754540521486, 127.02546694890695],
-        [37.49646391248451, 127.02675574250912],
-      ],
-      markers: [],
-      infowindow,
-      // methods
-      changeSize,
-      displayMarker,
-      displayInfoWindow,
-    };
-  },
-
-  methods: {},
+    script.src =
+      "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=6980627efdafc9b33ee3f2e602c8f9da";
+    document.head.appendChild(script);
+  }
 });
 </script>
 
