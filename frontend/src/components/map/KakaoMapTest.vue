1015a917f7b4c75224b8e382b9e2698da6cdb20e
juni <woajun@naver.com>
2022-07-07 19:04:43 +0900

    clean KakaoMapTest

--- frontend/src/components/map/KakaoMapTest.vue
+++ frontend/src/components/map/KakaoMapTest.vue
@@ -1,65 +1,28 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import KakaoMap from "./KakaoMap.vue";
+import KakaoContent from "./KakaoContent.vue";
 
-const center = ref();
-const changeSize = ref<(size: number) => void>();
-const displayMarker = ref<(markerPositions: any[]) => void>();
-const displayInfoWindow = ref<(info: any) => void>();
-
-const marker1 = [
-  { latitude: 33.452278, longitude: 126.567803 },
-  { latitude: 33.452671, longitude: 126.574792 },
-  { latitude: 33.451744, longitude: 126.572441 },
-];
-
-const marker2 = [
-  { latitude: 37.499590490909185, longitude: 127.0263723554437 },
-  { latitude: 37.499427948430814, longitude: 127.02794423197847 },
-  { latitude: 37.498553760499505, longitude: 127.02882598822454 },
-  { latitude: 37.497625593121384, longitude: 127.02935713582038 },
-  { latitude: 37.49629291770947, longitude: 127.02587362608637 },
-  { latitude: 37.49754540521486, longitude: 127.02546694890695 },
-  { latitude: 37.49646391248451, longitude: 127.02675574250912 },
-];
-
-const info = {
-  content: '<div style="padding:5px;">Hello World!</div>',
-  latitude: 33.450701,
-  longitude: 126.570667,
-};
-
-const content =
-  '<div class="wrap">' +
-  '    <div class="info">' +
-  '        <div class="title">' +
-  "            카카오 스페이스닷원" +
-  '            <div class="close" onclick="closeOverlay()" title="닫기"></div>' +
-  "        </div>" +
-  '        <div class="body">' +
-  '            <div class="img">' +
-  '                <img src="https://cfile181.uf.daum.net/image/250649365602043421936D" width="73" height="70">' +
-  "           </div>" +
-  '            <div class="desc">' +
-  '                <div class="ellipsis">제주특별자치도 제주시 첨단로 242</div>' +
-  '                <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>' +
-  '                <div><a href="https://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>' +
-  "            </div>" +
-  "        </div>" +
-  "    </div>" +
-  "</div>";
+const center = ref({ latitude: 33.452278, longitude: 126.567803 });
+// const content = ref<string>();
+// const contentRef = ref<InstanceType<typeof KakaoContent> | null>(null);
 
 function changeCenter(lat: number, lng: number) {
   center.value = { latitude: lat, longitude: lng };
 }
+
+// function logContent() {
+//   console.log(contentRef.value);
+//   console.log(contentRef.value?.$el);
+//   console.log(contentRef.value?.$el.outerHTML);
+//   contentRef.value?.open();
+//   content.value = contentRef.value?.$el.outerHTML as string;
+// }
 </script>
 <template>
   <div>
-    <KakaoMap
-      :center="center"
-      marker-type="custom-overlay"
-      :content="content"
-    />
+    <KakaoMap :center="center" marker-type="custom-overlay" />
+    <button @click="logContent">1번</button>
     <button @click="changeCenter(33.452278, 126.567803)">1번</button>
     <button @click="changeCenter(37.498553760499505, 127.02882598822454)">
       2번
