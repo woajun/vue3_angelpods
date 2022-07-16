ebaf81db27fd11243c5be94582a1e5ec82e4d39b
juni <woajun@naver.com>
2022-07-16 14:34:57 +0900

    unuse overlay

--- frontend/src/pages/find/ChoiceLocationModal.vue
+++ frontend/src/pages/find/ChoiceLocationModal.vue
@@ -21,33 +21,7 @@ defineProps<{
           ></button>
         </div>
         <div class="modal-body">
-          <KakaoMap :relayout="modelValue">
-            <template #overlay>
-              <div class="wrap">
-                <div class="info">
-                  <div class="title">
-                    {op.title}
-                    <div class="close" title="닫기"></div>
-                  </div>
-                  <div class="body">
-                    <div class="img">
-                      <img src="" width="73" height="70" />
-                    </div>
-                    <div class="desc">
-                      <div class="ellipsis">{op.message}</div>
-                      <div class="jibun ellipsis">{op.address}</div>
-                      <div>
-                        <a href="{op.url}" target="_blank" class="link"
-                          >홈페이지</a
-                        >
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </template>
-          </KakaoMap>
-
+          <KakaoMap :relayout="modelValue" />
           <MapSearcher />
         </div>
       </div>
---
+++ frontend/src/pages/find/ModalOverlay.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup></script>
+<template>
+  <div class="wrap">
+    <div class="info">
+      <div class="title">
+        {op.title}
+        <div class="close" title="닫기"></div>
+      </div>
+      <div class="body">
+        <div class="img">
+          <img src="" width="73" height="70" />
+        </div>
+        <div class="desc">
+          <div class="ellipsis">{op.message}</div>
+          <div class="jibun ellipsis">{op.address}</div>
+          <div>
+            <a href="{op.url}" target="_blank" class="link">홈페이지</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
