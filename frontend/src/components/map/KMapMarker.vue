d1ddbde0afa5e3a92377b3fa189864a7aad1ffea
juni <woajun@naver.com>
2022-07-16 18:52:23 +0900

    add marker ref

--- frontend/src/components/map/KMapMarker.vue
+++ frontend/src/components/map/KMapMarker.vue
@@ -6,41 +6,30 @@ interface Props {
   position: kakao.maps.LatLng | null;
 }
 const props = defineProps<Props>();
+const marker = ref<kakao.maps.Marker | null>(null);
 
 watch(
   () => props.map,
-  (map) => {
-    if (map) initMarker(map);
+  (newMap) => {
+    if (!newMap) return;
+    marker.value = new window.kakao.maps.Marker({
+      position: newMap.getCenter(),
+    });
   }
 );
 
-let marker: kakao.maps.Marker;
+watch(
+  () => props.position,
+  (latlng) => {
+    if (!props.map || !latlng) return;
+    rePosition(latlng);
+    marker.value?.setMap(props.map);
+  }
+);
 
-function initMarker(map: kakao.maps.Map) {
-  marker = new window.kakao.maps.Marker({
-    position: map.getCenter(),
-  });
-  marker.setMap(map);
-
-  kakao.maps.event.addListener(map, "click", clickEvent);
-
-  if (!infoWindow.value) return;
-  const infowindows = new window.kakao.maps.InfoWindow({
-    content: infoWindow.value,
-    removable: true,
-    position: map.getCenter(),
-  });
-
-  kakao.maps.event.addListener(marker, "click", () => {
-    infowindows.open(map, marker);
-  });
+function rePosition(latlng: kakao.maps.LatLng) {
+  marker.value?.setPosition(latlng);
 }
-
-function clickEvent(click: kakao.maps.event.MouseEvent) {
-  marker.setPosition(click.latLng);
-}
-
-const infoWindow = ref<HTMLDivElement>();
 </script>
 <template>
   <div ref="infoWindow">
