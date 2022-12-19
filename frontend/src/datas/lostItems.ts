a37012c64c965e667e7496e95ce794a7778f7c35
juni <woajun@naver.com>
2022-12-19 09:56:49 +0900

    defined lostIems spec in datas

---
+++ frontend/src/datas/lostItems.ts
@@ -0,0 +1,18 @@
+const lostItems = [
+  {
+    id: 1,
+    category: [
+      { id: 1, name: "무선이어폰" },
+      { id: 2, name: "에어팟" },
+      { id: 3, name: "2세대" },
+    ],
+    title: "에어팟주웠어요",
+    address: "서울시 응봉동",
+    detail: "",
+    coords: { lat: 32.214124, lng: 140.2424231 },
+    chat: 5,
+    date: "2022-12-19 15:33:27",
+  },
+];
+
+export default lostItems;
